08f788199910fc5911b5155946eeb1eff38289f2
Harry Amad <harryamad@gmail.com>
2025-09-05 07:19:53 +0000

    bandwidth sweeps with new simple lagrange potential

--- semicircles_nlot_metric.sh
+++ semicircles_nlot_metric.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-seeds=(0 1 2 3 4 5 6 7 8 9)
+seeds=(0 1 2 3 4)
 
 for seed in "${seeds[@]}"; do
+  python NLOT/train.py dataset='conditional_semicircles' geometry='neural_net_metric' include_inverse_potential=True bandwidth=0.5 lambda=0.05 wandb_project="semicircles_nlot_metric" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
   python NLOT/train.py dataset='conditional_semicircles' geometry='neural_net_metric' include_inverse_potential=True bandwidth=0.4 lambda=0.05 wandb_project="semicircles_nlot_metric" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
+  python NLOT/train.py dataset='conditional_semicircles' geometry='neural_net_metric' include_inverse_potential=True bandwidth=0.3 lambda=0.05 wandb_project="semicircles_nlot_metric" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
+  python NLOT/train.py dataset='conditional_semicircles' geometry='neural_net_metric' include_inverse_potential=True bandwidth=0.2 lambda=0.05 wandb_project="semicircles_nlot_metric" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
+  python NLOT/train.py dataset='conditional_semicircles' geometry='neural_net_metric' include_inverse_potential=True bandwidth=0.1 lambda=0.05 wandb_project="semicircles_nlot_metric" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
+  python NLOT/train.py dataset='conditional_semicircles' geometry='neural_net_metric' include_inverse_potential=True bandwidth=0.05 lambda=0.05 wandb_project="semicircles_nlot_metric" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
+
 done
