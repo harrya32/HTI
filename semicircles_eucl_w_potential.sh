5a5cca58ed0ec506f1f4edf89ac17949f1329c1b
Harry Amad <harryamad@gmail.com>
2025-09-06 14:53:20 +0000

    more iterations at bw 0.05 for semicircles

--- semicircles_eucl_w_potential.sh
+++ semicircles_eucl_w_potential.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-seeds=(0 1 2 3 4)
+seeds=(5 6 7 8 9 10 11 12 13 14 15 16 17 18 19)
 
 for seed in "${seeds[@]}"; do
-  python NLOT/train.py dataset='conditional_semicircles' geometry='sq_euclidean_manifold' include_inverse_potential=True bandwidth=0.5 lambda=0.05 wandb_project="semicircles_eucl_w_potential" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
-
-  python NLOT/train.py dataset='conditional_semicircles' geometry='sq_euclidean_manifold' include_inverse_potential=True bandwidth=0.4 lambda=0.05 wandb_project="semicircles_eucl_w_potential" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
-  
-  python NLOT/train.py dataset='conditional_semicircles' geometry='sq_euclidean_manifold' include_inverse_potential=True bandwidth=0.3 lambda=0.05 wandb_project="semicircles_eucl_w_potential" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
-
-  python NLOT/train.py dataset='conditional_semicircles' geometry='sq_euclidean_manifold' include_inverse_potential=True bandwidth=0.2 lambda=0.05 wandb_project="semicircles_eucl_w_potential" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
-
-  python NLOT/train.py dataset='conditional_semicircles' geometry='sq_euclidean_manifold' include_inverse_potential=True bandwidth=0.1 lambda=0.05 wandb_project="semicircles_eucl_w_potential" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
-
   python NLOT/train.py dataset='conditional_semicircles' geometry='sq_euclidean_manifold' include_inverse_potential=True bandwidth=0.05 lambda=0.05 wandb_project="semicircles_eucl_w_potential" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
 
 done
