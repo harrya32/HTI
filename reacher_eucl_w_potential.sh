4e830aa9d1691a024cc4ee600fba49fb67df7fa9
Harry Amad <harryamad@gmail.com>
2025-09-12 10:54:08 +0000

    testing higher bandwidths for reacher

--- reacher_eucl_w_potential.sh
+++ reacher_eucl_w_potential.sh
@@ -3,5 +3,5 @@
 seeds=(0 1 2 3 4)
 
 for seed in "${seeds[@]}"; do
-  python NLOT/train.py dataset='reacher_data' geometry='sq_euclidean_manifold' include_inverse_potential=True bandwidth=0.5 conditional_bandwidth=1.0 lambda=0.001 wandb_project="reacher_eucl_w_potential" ctransform_solver.max_iter=3 D=2 C=11 categorical=False seed=$seed
+  python NLOT/train.py dataset='reacher_data' geometry='sq_euclidean_manifold' include_inverse_potential=True bandwidth=0.75 conditional_bandwidth=1.0 lambda=0.001 wandb_project="reacher_eucl_w_potential" ctransform_solver.max_iter=3 D=2 C=11 categorical=False seed=$seed
 done
