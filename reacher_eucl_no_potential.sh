fed960aa66a3ecfc58e4330908cec60885606030
Harry Amad <harryamad@gmail.com>
2025-09-20 19:30:13 +0000

    running more reacher iterations for learned and eucl

---
+++ reacher_eucl_no_potential.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+seeds=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19)
+
+for seed in "${seeds[@]}"; do
+  python NLOT/train.py dataset='reacher_data' geometry='sq_euclidean_manifold' include_inverse_potential=False wandb_project="reacher_eucl_no_potential" ctransform_solver.max_iter=3 D=2 C=11 categorical=False target_potential_dim_hidden=[64,64,64,64] source_map_dim_hidden=[64,64,64,64] seed=$seed
+done
--- reward_weighting_ours.sh
+++ reward_weighting_ours.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-seeds=(13 15)
+seeds=(5 6 7 8 9 12 13 15)
 
 for seed in "${seeds[@]}"; do
   python NLOT/train.py dataset='reward_weighting_data' geometry='neural_net_metric_eig' include_inverse_potential=True bandwidth=0.1 conditional_bandwidth=1.0 lambda=0.0001 wandb_project="reward_weighting_learned_w_potential" ctransform_solver.max_iter=3 D=2 C=7 categorical=False target_potential_dim_hidden=[64,64,64,64] source_map_dim_hidden=[64,64,64,64] seed=$seed
