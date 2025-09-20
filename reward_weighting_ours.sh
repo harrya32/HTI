db89c1b3bcf8a5d64256417fe33cf5c16dd988dd
Harry Amad <harryamad@gmail.com>
2025-09-20 16:44:35 +0000

    testing film conditioning for cancer baselines, and same num hidden dimensions, and nlot on cancer/reacher

--- reward_weighting_ours.sh
+++ reward_weighting_ours.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-seeds=(5 6 7 8 9 10 11 12 13 14 15 16 17 18 19)
+seeds=(13 15)
 
 for seed in "${seeds[@]}"; do
   python NLOT/train.py dataset='reward_weighting_data' geometry='neural_net_metric_eig' include_inverse_potential=True bandwidth=0.1 conditional_bandwidth=1.0 lambda=0.0001 wandb_project="reward_weighting_learned_w_potential" ctransform_solver.max_iter=3 D=2 C=7 categorical=False target_potential_dim_hidden=[64,64,64,64] source_map_dim_hidden=[64,64,64,64] seed=$seed
