2f9921660ca97b65342c2f80bca6d07fd3e77b94
Harry Amad <harryamad@gmail.com>
2025-11-12 22:50:43 +0000

    more pooladian runs

--- semicircles_pooladian.sh
+++ semicircles_pooladian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-seeds=(0 1 2 3 4)
+seeds=(5 6 7 8 9 10 11 12 13 14 15 16 17 18 19)
 
 for seed in "${seeds[@]}"; do
   python NLOT/train.py dataset='conditional_semicircles' geometry='neural_net_metric' include_inverse_potential=False wandb_project="semicircles_pooladian" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=4 seed=$seed
