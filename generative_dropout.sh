e666c050f46c5db3cafaefbd7ec99f9abfb59693
Harry Amad <harryamad@gmail.com>
2026-03-02 12:14:13 +0000

    more generative dropout runs, at lambda = 0.01 for HTI

--- generative_dropout.sh
+++ generative_dropout.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-seeds=(3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19)
+seeds=(13 14 15 16 17 18 19)
 
 for seed in "${seeds[@]}"; do
   python NLOT/train.py dataset='2moons_dropout' geometry='neural_net_metric_eig' include_inverse_potential=True bandwidth=0.2 conditional_bandwidth=1.0 lambda=0.01 wandb_project="2moons_learned_w_potential" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=2 target_potential_dim_hidden=[64,64,64,64] source_map_dim_hidden=[64,64,64,64] seed=$seed
 
   #python NLOT/train.py dataset='2moons_dropout' geometry='sq_euclidean_manifold' include_inverse_potential=False wandb_project="2moons_eucl_no_potential" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=2 target_potential_dim_hidden=[64,64,64,64] source_map_dim_hidden=[64,64,64,64] seed=$seed
 
-  python NLOT/train.py dataset='2moons_dropout' geometry='sq_euclidean_manifold' include_inverse_potential=True bandwidth=0.2 conditional_bandwidth=1.0 lambda=0.01 wandb_project="2moons_eucl_w_potential" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=2 target_potential_dim_hidden=[64,64,64,64] source_map_dim_hidden=[64,64,64,64] seed=$seed
+  #python NLOT/train.py dataset='2moons_dropout' geometry='sq_euclidean_manifold' include_inverse_potential=True bandwidth=0.2 conditional_bandwidth=1.0 lambda=0.01 wandb_project="2moons_eucl_w_potential" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=2 target_potential_dim_hidden=[64,64,64,64] source_map_dim_hidden=[64,64,64,64] seed=$seed
 
   #python NLOT/train.py dataset='2moons_dropout' geometry='neural_net_metric_eig' include_inverse_potential=False wandb_project="2moons_learned_no_potential" ctransform_solver.max_iter=10 D=2 C=1 categorical=True num_categories=2 target_potential_dim_hidden=[64,64,64,64] source_map_dim_hidden=[64,64,64,64] seed=$seed
 
